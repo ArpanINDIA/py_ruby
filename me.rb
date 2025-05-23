9aa5d0342cdd23b756305afedbbc737a932fe705
Arpan Mondal <arpanarpan154@gmail.com>
2025-05-23 12:39:42 +0530

    first commit

---
+++ me.rb
@@ -0,0 +1,59 @@
+puts "hello"
+puts "2 + 3 = #{2 + 3}"
+puts "10 - 4 = #{10 - 4}"
+puts "5 * 6 = #{5 * 6}"
+puts "20 / 4 = #{20 / 4}"
+
+# A simple Ruby program demonstrating classes, methods, loops, and user input
+
+class Calculator
+  def add(a, b)
+    a + b
+  end
+
+  def subtract(a, b)
+    a - b
+  end
+
+  def multiply(a, b)
+    a * b
+  end
+
+  def divide(a, b)
+    return "Cannot divide by zero" if b == 0
+    a.to_f / b
+  end
+end
+
+calc = Calculator.new
+
+puts "Welcome to the Ruby Calculator!"
+loop do
+  puts "\nChoose operation: add, subtract, multiply, divide, or exit"
+  print "Operation: "
+  op = gets.chomp.downcase
+
+  break if op == "exit"
+
+  print "Enter first number: "
+  a = gets.chomp.to_f
+  print "Enter second number: "
+  b = gets.chomp.to_f
+
+  result = case op
+           when "add"
+             calc.add(a, b)
+           when "subtract"
+             calc.subtract(a, b)
+           when "multiply"
+             calc.multiply(a, b)
+           when "divide"
+             calc.divide(a, b)
+           else
+             "Unknown operation"
+           end
+
+  puts "Result: #{result}"
+end
+
+puts "Goodbye!"
